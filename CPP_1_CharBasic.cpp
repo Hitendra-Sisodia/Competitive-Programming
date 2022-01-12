f952c09f8babda47e67675018aca8b4360cde572
Hitendra Sisodia <hitendrasisodia18@gmail.com>
2022-01-12 16:43:46 +0530

    Character Basic

---
+++ CPP_1_CharBasic.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int a,b;
+    char c,d;
+    
+    cin>>c;
+    cout<<c<<endl;        //prints its string value in form of an integer
+    cout<<int(c)<<endl;;  //prints its asscci values also known as type casting
+
+    cout<<d<<" "<<a<<endl;  //string input only one value  i/p--> 1234 232 o/p--> 1 234 
+
+    return 0;
+}
